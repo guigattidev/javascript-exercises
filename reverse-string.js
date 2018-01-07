1c05af295e6ba1f1df3cee9330c839173d262aaf
guigattidev <gattiwhoomps@hotmail.com>
2018-01-07 17:03:24 -0200

    Finish reverse string exercise

---
+++ reverse-string.js
@@ -0,0 +1,16 @@
+// Problem:
+// Reverse the string "Hello World!" for "!dlroW olleH" in the simplest possible way.
+
+// Teste:
+describe("reverse string function", function() {
+    
+  it("it should to reserve the string", function() {
+    expect(reverseString("Hello World!")).toBe("!dlroW olleH");
+  });
+
+});
+
+// Solution:
+function reverseString(string) {
+  return string.split("").reverse().join("");
+}
