9caccae43e4544f12f0fb449d4eaed24631ff12a
guigattidev <gattiwhoomps@hotmail.com>
2018-01-07 16:35:36 -0200

    Test factorial number exercise

--- factorial-number.js
+++ factorial-number.js
@@ -1,6 +1,19 @@
 // Problem:
 // Use the function factorialNumber(number) as recursive function to complete the exercise and return the result.
 
+// Teste:
+describe("factorial number function", function() {
+    
+  it("if the argument equal to 1 should return 1", function() {
+    expect(factorialNumber(1)).toEqual(1);
+  });
+ 
+  it("if the argument equal to 5 should return 120", function() {
+    expect(factorialNumber(5)).toEqual(120);
+  });
+
+});
+
 // Solution:
 function factorialNumber(number) {
     if (number === 0) {
